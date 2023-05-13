a5db9e2075a3b686c314c08d48bcf9df72b64185
Sara Szwejkowska <saraszw@op.pl>
2023-05-13 15:35:59 +0200

    new files and created routing

---
+++ components/nav/TopNav.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="nav">
+    <div class="left-nav item2">
+      <span><NuxtLink to="/">Strona domowa</NuxtLink></span>
+      <span><NuxtLink to="/onas">O nas</NuxtLink></span>
+    </div>
+  </div>
+</template>
+
+<script></script>
+<style scoped>
+.nav {
+  display: flex;
+  /* flex-direction: column; */
+
+  background-color: black;
+  height: 100%;
+  width: 100%;
+  max-height: 200px;
+  min-height: 200px;
+  max-width: 100vw;
+  min-width: 200px;
+}
+
+.left-nav {
+
+  max-height: 100px;
+  min-height: 100px;
+  max-width: 50px;
+  min-width: 50px;
+}
+
+/* px, rem, em, vh, vw, % */
+.item {
+
+  max-height: 100px;
+  min-height: 100px;
+  max-width: 100px;
+  min-width: 100px;
+}
+.item2 {
+  display: flex;
+  flex-direction: column;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <TopNav/>
+    <bigImage image="https://img.freepik.com/darmowe-zdjecie/zolty-zonkil-narcyz-kwitnacy-w-ogrodzie_127675-2729.jpg?w=826&t=st=1679759619~exp=1679760219~hmac=16bc530ed103e815ea90b0515168ca251845e2dcf4822f3f3cba293d01b28582"/>
+    <bigImage image="https://img.freepik.com/darmowe-psd/wiosna-kwiatowy-tlo-podium_23-2150154136.jpg?w=740&t=st=1679759809~exp=1679760409~hmac=397d6d3bda0afec08cd704055ac0eec8f2a8c5f758b1327d1a0b2b34b0e29d06"/>
+  </div>
+</template>
+
+<script>
+import TopNav from '../components/nav/TopNav.vue'
+import bigImage from '../components/gallery/bigImage.vue'
+export default {
+  name: 'IndexPage',
+  components: { TopNav, bigImage }
+}
+</script>
